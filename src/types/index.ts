97e7277aa3e89835d0d2af30b09cc719d8772f56
Egor Kiselev <eakiselev@yandex.ru>
2025-05-20 17:51:50 +0300

    feat types: added data types

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+// Товар
+
+interface IProduct {
+	id: string;
+    description: string;
+    image: string;
+    title: string;
+	category: string;
+    price: number | null;
+}
+
+// Заказ
+
+type TOrderPayment = 'cash' | 'card';
+
+interface IOrder {
+	items: IProduct[];
+	payment: TOrderPayment;
+	address: string;
+	email: string;
+	phone: string;
+}
+
+type TOrderStep = 'shipment' | 'contacts';
+
+type TOrderInvoice = Omit<IOrder, 'items'> & {
+	items: string[];
+	total: number;
+};
+
+interface IOrderResult {
+	id: string;
+	total: number;
+}
+
+export { IProduct, TOrderInvoice, TOrderPayment, TOrderStep, IOrder, IOrderResult }
